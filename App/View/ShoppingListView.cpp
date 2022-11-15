e6e9537226cf1e8da41773526a6be09bd23b9490
guilian pipart <guilian.pipart@gmail.com>
2022-11-15 12:16:04 +0100

    organize Shopping list view

--- App/View/ShoppingListView.cpp
+++ App/View/ShoppingListView.cpp
@@ -11,32 +11,63 @@ ShoppingListView::ShoppingListView()
 	this->setBackgroundColor(82, 89, 92);
 	this->setFont(App::getSDLDefaultFont());
 
-	Rect* rectTitle = new Rect();
+	//title part
 	{
-		rectTitle->setPosition(0, 70);
-		rectTitle->setSize(750, 110);
-		rectTitle->setColor(181, 222, 255);
-		this->addWidget(rectTitle);
+		Rect* rectTitle = new Rect();
+		{
+			rectTitle->setPosition(0, 70);
+			rectTitle->setSize(750, 110);
+			rectTitle->setColor(181, 222, 255);
+			this->addWidget(rectTitle);
+		}
+
+		Text* title = new Text();
+		{
+			title->setPosition(10, 105);
+			title->setText("SHOPPING LIST");
+			title->setColor(0, 0, 0);
+			this->addWidget(title);
+			title->setHorizontallyCentered();
+		}
 	}
 
-	Text* title = new Text();
+	//body part
 	{
-		title->setPosition(10, 105);
-		title->setText("SHOPPING LIST");
-		title->setColor(0, 0, 0);
-		this->addWidget(title);
-		title->setHorizontallyCentered();
+		
 	}
 
-	Button* returnBtn = new Button("Return");
+	//button part
 	{
-		returnBtn->setPosition(40, 700);
-		returnBtn->setSize(BUTTON_WIDTH, BUTTON_HEIGHT);
-		returnBtn->setColor(220, 220, 220);
-		returnBtn->setOnClickCallback([]() {
-			App::setCurrentView(App::getViewMainMenu());
-			});
-		this->addWidget(returnBtn);
-		returnBtn->setHorizontallyCentered();
+		Button* addBtn = new Button("Add");
+		{
+			addBtn->setPosition(40, 640);
+			addBtn->setSize(195, BUTTON_HEIGHT);
+			addBtn->setColor(181, 222, 255);
+			addBtn->setOnClickCallback([]() {
+				});
+			this->addWidget(addBtn);
+		}
+
+		Button* modifyBtn = new Button("Modify");
+		{
+			modifyBtn->setPosition(245, 640);
+			modifyBtn->setSize(195, BUTTON_HEIGHT);
+			modifyBtn->setColor(181, 222, 255);
+			modifyBtn->setOnClickCallback([]() {
+				});
+			this->addWidget(modifyBtn);
+		}
+
+		Button* returnBtn = new Button("Return");
+		{
+			returnBtn->setPosition(40, 700);
+			returnBtn->setSize(BUTTON_WIDTH, BUTTON_HEIGHT);
+			returnBtn->setColor(220, 220, 220);
+			returnBtn->setOnClickCallback([]() {
+				App::setCurrentView(App::getViewMainMenu());
+				});
+			this->addWidget(returnBtn);
+			returnBtn->setHorizontallyCentered();
+		}
 	}
 }
