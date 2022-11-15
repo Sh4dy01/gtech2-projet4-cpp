80a62ea6af1c42a45bdae010add558384a150a99
Lum0o <ProGpipart@gmail.com>
2022-11-15 17:43:23 +0100

    Shopping list view Ui

--- App/View/ShoppingListView.h
+++ App/View/ShoppingListView.h
@@ -9,4 +9,12 @@ class ShoppingListView : public View
 public:
 
 	ShoppingListView();
+	void setList(const char* tab);
+	void setIsModify();
+
+private:
+
+	const char* list[5];
+	int lengthList = 2;
+	bool isModify;
 };
